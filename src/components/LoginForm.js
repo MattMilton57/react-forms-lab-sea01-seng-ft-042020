706653406d0a25f8004b50363484acdc2b4e92a2
github username <github email address>
2020-07-27 16:27:30 -0700

    done

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,48 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username:"",
+      password:""
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.id]:e.target.value
+    })    
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+    // var username = this.state.username
+    if (this.state.username.length > 0 && this.state.password.length > 0){ this.props.handleLogin((this.state.username), (this.state.password))
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={e => this.submitHandler(e)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input 
+              id="username" 
+              name="username" 
+              type="text"
+              value={this.state.username}
+              onChange={e => this.changeHandler(e)} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input 
+              id="password" 
+              name="password" 
+              type="password"
+              value={this.state.password}
+              onChange={e => this.changeHandler(e)} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,34 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: "",
+      remainingCharacters:280
+    };
   }
+
+  handleTweet = (e) => {
+    // console.log(e.target.value.length)
+    // console.log(this.state.tweet.length)
+    this.setState({
+      tweet:e.target.value,
+      remainingCharacters:(this.props.maxChars - e.target.value.length)
+    })
+  }
+
+  remaining
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+          type="text" 
+          name="message" 
+          id="message" 
+          onChange={event => this.handleTweet(event)}
+          value={this.state.tweet} />
+          <strong>{this.state.remainingCharacters}</strong>
       </div>
     );
   }
